f35bae1f354d6bd30f81ce0e43f9fbea7815fffc
D. M. K. K. DISSANAYAKE <kalmithdissanayake@gmail.com>
2026-07-25 02:23:31 +0530

    add assignment table/model

---
+++ Order_MS/Models/TransportAssignment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Order_MS.Models;
+
+public partial class TransportAssignment
+{
+    public int AssignmentId { get; set; }
+
+    public int OrderId { get; set; }
+
+    public int VehicleId { get; set; }
+
+    public int DriverId { get; set; }
+
+    public DateTime AssignedOn { get; set; }
+
+    public string? Status { get; set; }
+
+    public virtual Driver Driver { get; set; } = null!;
+
+    public virtual Order Order { get; set; } = null!;
+
+    public virtual Vehicle Vehicle { get; set; } = null!;
+}

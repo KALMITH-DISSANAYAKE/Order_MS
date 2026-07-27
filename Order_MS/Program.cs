9d9b426dd69c9dd87fe1d2b3585bd8682b3888d5
Anuruth Kaushalya <2023cs089@stu.ucsc.cmb.ac.lk>
2026-07-28 00:57:17 +0530

    order-request added with order create part

--- Order_MS/Program.cs
+++ Order_MS/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
 using Order_MS.Interfaces;
+using Order_MS.Sevices;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -60,10 +61,10 @@ builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepositor
 
 // Your services
 builder.Services.AddScoped<IAuthService, AuthService>();
-//builder.Services.AddScoped<IBranchService, BranchService>();
-//builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IBranchService, BranchService>();
+builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IOrderRequestService, OrderRequestService>();
-//builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IInventoryService, InventoryService>();
 builder.Services.AddScoped<ITransportService, TransportService>();
 builder.Services.AddScoped<IDeliveryService, DeliveryService>();
@@ -71,6 +72,7 @@ builder.Services.AddScoped<IDeliveryService, DeliveryService>();
 // Generic repo 
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
+builder.Services.AddScoped(typeof(IOrderRepository<>), typeof(OrderRepository<>));
 // Specific repo (transport management)
 builder.Services.AddScoped<ITransportAssignmentRepository, TransportAssignmentRepository>();
 
